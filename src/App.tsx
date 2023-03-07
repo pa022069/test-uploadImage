3bfebc2f8571d1c4a1c03e17a98f15317a28e0b9
Wistronits <pa022069@gmail.com>
2023-03-07 16:58:22 +0800

    fix upload error

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 const App = (): JSX.Element => {
   const uploadFile = async (e: any) => {
-    if (e.target.files && e.target.files.length === 0) return;
+    if (!e.target.files || e.target.files.length === 0) return;
     const file = e.target.files[0];
     const formData = new FormData();
-    formData.append('imageSrc', file);
+    formData.append("imageSrc", file);
     try {
       const response = await axios({
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-        url: 'http://localhost:4001/api',
-        data: formData
-      })
-      console.log('response', response)
+        method: "POST",
+        url: "http://localhost:4001/api",
+        data: formData,
+      });
+      console.log("response", response);
     } catch (err: any) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div
       style={{
-        width: '100%',
-        height: '300px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
+        width: "100%",
+        height: "300px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <input type="file" accept="image/*" onChange={uploadFile} />
     </div>
   );
-}
+};
 
 export default App;
